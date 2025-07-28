98b370d2c707a73fc745697ccea8293132281c34
Ruhollah Jafari <r.jafari@p2run.com>
2025-07-28 12:42:52 +0330

    Use Env and body parser at middleware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const  express  = require('express');
+const  morgan  = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 // import routes
 const authRoutes = require('./routes/auth') 
  
-// middleware
+// app middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//app.use(cors());
+if(process.env.NODE_ENV='development'){
+  app.use(cors({origin:`http://localhost:3000`}))  
+}
+// middlewares
 app.use('/api',authRoutes);
-const port =process.env.port || 8000
+
+
+const port =process.env.PORT || 8000
 app.listen(port,()=>{
-console.log(`API is running on port ${port}`);
+console.log(`API is running on port ${port} - ${process.env.NODE_ENV}`);
 });
 
 
